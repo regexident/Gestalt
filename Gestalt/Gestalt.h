dcacf6e218db01805bd8df23a035b8e7cd58397a
Vincent Esche <regexident@gmail.com>
2017-07-03 18:07:19 +0200

    Initial Commit

---
+++ Gestalt/Gestalt.h
@@ -0,0 +1,19 @@
+//
+//  Gestalt.h
+//  Gestalt
+//
+//  Created by Vincent Esche on 7/3/17.
+//  Copyright © 2017 Vincent Esche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Gestalt.
+FOUNDATION_EXPORT double GestaltVersionNumber;
+
+//! Project version string for Gestalt.
+FOUNDATION_EXPORT const unsigned char GestaltVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gestalt/PublicHeader.h>
+
+
