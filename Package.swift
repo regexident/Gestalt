f13f1b7d0e94d3b729fba37b2f4262aaa592b551
Helge Heß <me@helgehess.eu>
2018-05-14 16:24:49 +0200

    SPM: Properly export Gestalt product
    ... as a library.

--- Package.swift
+++ Package.swift
@@ -3,10 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "Gestalt",
-
-    dependencies: [
+    products: [
+        .library(name: "Gestalt", targets: [ "Gestalt" ])
     ],
-
+    dependencies: [],
     targets: [
         .target    (name         : "Gestalt", 
                     path         : "Gestalt"),
