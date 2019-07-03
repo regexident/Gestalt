db6e51e0e2ca229388b9007bed22866494e2a81d
Vincent Esche <regexident@gmail.com>
2019-07-03 20:33:27 +0200

    Improved multi-platform support

--- Gestalt/ThemeObserver.swift
+++ Gestalt/ThemeObserver.swift
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#if os(iOS)
+#if os(iOS) || os(watchOS) || os(tvOS)
 import UIKit
 #elseif os(OSX)
 import AppKit
